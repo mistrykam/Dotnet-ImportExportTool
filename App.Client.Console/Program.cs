cb51feb8b7620f14a4831e9dea22e5afa15766f2
Kam Mistry <mistrykam@gmail.com>
2021-03-24 11:51:54 -0400

    Removed the logger and setting debug in Program main method.

--- App.Client.Console/Program.cs
+++ App.Client.Console/Program.cs
@@ -11,19 +11,11 @@ namespace App.Client.Console
     {
         private static async Task Main(string[] args)
         {
+            // startup configuration
             IServiceCollection services = new ServiceCollection();
             Startup startup = new Startup(args);
             startup.ConfigureServices(services);
-
             IServiceProvider serviceProvider = services.BuildServiceProvider();
-
-            // test it
-            ILogger<Program> logger = serviceProvider.GetService<ILogger<Program>>();
-            logger.LogInformation($"Hello World {System.DateTime.Now}");
-
-            // setting
-            AppSettings setting = serviceProvider.GetService<AppSettings>();
-            logger.LogInformation($"User name is {setting.UserName} Password is {setting.Password}");
 
             // GIT Export request
             GitFileExporter git = serviceProvider.GetService<GitFileExporter>();
