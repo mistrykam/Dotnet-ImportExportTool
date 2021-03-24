a38294485aa9d1881f436f34c5d1b627042ede45
Kam Mistry <mistrykam@gmail.com>
2021-03-24 11:53:16 -0400

    Removed UserName and Password from AppSettings, not used, for inital test.

--- App.Core.Domain/Entities/ApplicationSettings.cs
+++ App.Core.Domain/Entities/ApplicationSettings.cs
@@ -2,9 +2,6 @@
 {
     public class AppSettings
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-
         public string LogFilePath { get; set; }
 
         public string GitUri { get; set; }
